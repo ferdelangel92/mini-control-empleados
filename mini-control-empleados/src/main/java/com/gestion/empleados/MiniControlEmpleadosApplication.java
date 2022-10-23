705f33c9534895b58c864e683d26cc822517ce25
Howard Fernando Del Angel Leija <ferdelangel92@gmail.com>
2022-10-23 08:52:15 -0400

    java crud

---
+++ mini-control-empleados/src/main/java/com/gestion/empleados/MiniControlEmpleadosApplication.java
@@ -0,0 +1,12 @@
+package com.gestion.empleados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniControlEmpleadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniControlEmpleadosApplication.class, args);
+	}
+}
